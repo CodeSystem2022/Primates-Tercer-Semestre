6cf699cdcc142849f05d3c4b35db7af0137a1d41
Reinoso98 <111932493+Reinoso98@users.noreply.github.com>
2023-06-06 22:04:54 -0300

    Delete pruebas 1_Agustin Reinoso.java

--- Java/Class_VII/pruebas 1_Agustin Reinoso.java
+++
@@ -1,8 +0,0 @@
-package test;
-import accesodatos.*;
-public class TestInterfaces{
-    public static void main(String[] args) {
-        IAcceso Datos datos = new Implementacion MySql();
-        datos.listar(); 
-    }
-}
